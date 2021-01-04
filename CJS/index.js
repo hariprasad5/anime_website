76dfceddf122e614db6d9aec8a009aa6d656183a
hariprasad5 <hariprasadms5@gmail.com>
2021-01-04 22:35:35 +0530

    change cjs

--- CJS/index.js
+++ CJS/index.js
@@ -1,28 +1,14 @@
-// Use onclick=navpic
-let pageChange =  function () {
-    window.location.href = "https://www.desuzone.com/2020/01/anime-movie-2021.html";
+function openPage(pageName, elmnt) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.querySelectorAll(".tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.querySelectorAll(".tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    document.getElementsByClassName(pageName)[i].style.display = "block";
   }
 
-  let authorFunc1 = function () {
-    window.location.href = "https://www.google.com/search?sxsrf=ALeKk00YVrcIUYTZGgF_Zxq9rM1yvPq8IA%3A1607685419443&ei=K1XTX_3NGsrerQHHz6CwAQ&q=Osamu+Tezuka&oq=Osamu+Tezuka&gs_lcp=CgZwc3ktYWIQDDIHCCMQyQMQJzIHCC4QsQMQQzIHCAAQFBCHAjIECAAQQzICCAAyAggAMgIIADICCAAyAggAMgIILlCikwJYopMCYJqcAmgAcAB4AIABaIgBaJIBAzAuMZgBAKABAqABAaoBB2d3cy13aXrAAQE&sclient=psy-ab&ved=0ahUKEwi9j9jy5sXtAhVKbysKHccnCBYQ4dUDCA0";
-  }
+}
 
-  let authorFunc2 = function () {
-    window.location.href = "https://www.google.com/search?sxsrf=ALeKk01kkn6tZnxMOxploqEawh_IYY9V0w:1607685456635&q=Eiichiro+Oda&stick=H4sIAAAAAAAAAONQFuLUz9U3MCzLMTRXgjDjU-JNTjEihE8xcoDZxtm5MGGIErCwWU6JOUzYqNIwA8o2MTRJKYaxTaoKs38xagWl5iSWpKYolOQr-Bcn5pYqhKRWlWYnKiTmpSh4ZBblA0UcixKzE8sTd7EgrIeywYYsYuVxzcxMzgAqVvBPSZzAxniLTZJBa2oVV2rL-ue5n5P5Tf8cTLCNjlOyuelZAADll_UI3QAAAA&sa=X&ved=2ahUKEwiqmLaE58XtAhWiqksFHcSIAFgQ-BYwLHoECBIQOA&biw=1536&bih=722";
-  }
-
-  let authorFunc3 = function () {
-    window.location.href = "https://www.google.com/search?sa=X&sxsrf=ALeKk026RE_wLvmjIZ03OjfIC-lpjyKedw:1607685582632&q=Akira+Toriyama&stick=H4sIAAAAAAAAAONQFuLUz9U3MCzLMTRX4gAzjbNzTzEiRE8xYgjHp8SbQIXNckrMYcJGlYYZULaJoUlKMYxtUlWY_YtRKyg1J7EkNUWhJF_Bvzgxt1QhJLWqNDtRITEvRcEjsygfKOJYlJidWJ64iwVhPZQNNmQRK59jdmZRokJIflFmZWJu4gQ2xltskgxaU6u4UlvWP8_9nMxv-udggm10nJLNTc8CAHx1taTeAAAA&ved=2ahUKEwj0sMDA58XtAhVc6nMBHZm9CzUQ-BYwKHoECBIQNQ&biw=1536&bih=722#";
-  }
-
-  let authorFunc4 = function () {
-    window.location.href = "https://www.google.com/search?sa=X&sxsrf=ALeKk02xzRz8oN3sj5cu0gRgg8I9wJHGwg:1607685547840&q=Masashi+Kishimoto&stick=H4sIAAAAAAAAAONQFuLUz9U3MCzLMTRXAjNNDE1Sik8xIoRPMXKA2cbZuTDh-JR4E6iwWU6JOUzYqNIwA8pGNsXEpKow-xejVlBqTmJJaopCSb6Cf3FibqlCSGpVaXaiQmJeioJHZlE-UMSxKDE7sTxxFwvCeigbbMgiVkHfxOLE4oxMBe9MIJmbX5I_gY3xFpskg9bUKq7UlvXPcz8n85v-OZhgGx2nZHPTswAAnBkUFuIAAAA&ved=2ahUKEwj78fSv58XtAhUBhuYKHW71D08Q-BYwK3oECBQQQQ&cshid=1607685584961078&biw=1536&bih=722";
-  }
-
-  let authorFunc5 = function () {
-    window.location.href = "https://www.google.com/search?biw=1536&bih=722&sxsrf=ALeKk01NzUHQ0Ngmg6tJZnOpYhLchAblAQ%3A1607685623791&ei=91XTX-_sL_m-3LUPztCO2AU&q=yusuke+murata&oq=yukuse&gs_lcp=CgZwc3ktYWIQARgAMgoIIxCxAhDJAxAnMgcILhCxAxBDMgQIABAKMgQIABAKMgQIABAKMgQIABAKMgQILhAKMgQILhAKMgQILhAKMgQIABAKOgQIIxAnOgcILhDJAxBDOgQILhBDOgQIABBDOgcIIxDqAhAnOggIABCxAxCDAToKCC4QyQMQQxCTAjoFCC4QsQM6CAguELEDEIMBOggILhDHARCvAToCCAA6BwgAELEDEAo6AgguOg0IABCxAxCDARDJAxAKOgcILhCxAxAKOgoIABCxAxCDARAKOgoILhCxAxCDARAKULOrAViP9QFgv4ACaAFwAHgAgAGWAYgBiweSAQMwLjeYAQCgAQGqAQdnd3Mtd2l6sAEKwAEB&sclient=psy-ab";
-  }
-
-  let authorFunc6 = function () {
-    window.location.href = "https://www.google.com/search?biw=1536&bih=722&sxsrf=ALeKk022WZMJ0GvDOWjiCFhzMUcqhsmhkg%3A1607685657586&ei=GVbTX7KmI_O1mgfipp-AAg&q=hiromu+arakawa&gs_ssp=eJzj4tTP1TcwMakqzDZg9OLLyCzKzy1VSCxKzE4sTwQAcf4I4g&oq=hirom&gs_lcp=CgZwc3ktYWIQARgBMgcIIxDJAxAnMgoILhCxAxCDARBDMgIIADICCC4yAggAMgQIABAKMgIILjICCAAyBAguEEMyAggAOgQIIxAnOgcILhDJAxBDOgQIABBDOgcILhCxAxBDUIS3AVinzgFgytwBaABwAHgAgAGiAYgBlQWSAQMwLjWYAQCgAQGqAQdnd3Mtd2l6wAEB&sclient=psy-ab";
-  }
+document.getElementById("animeTopTab").click();
